ecf32f094537ce40c8e0387baa6a88afd130ae14
rene rodriguez <reneclavijo@live.com>
2021-07-22 10:45:21 -0500

    agregando el inventario de productos

--- reforzamiento2/main.rb
+++ reforzamiento2/main.rb
@@ -4,3 +4,17 @@
 
 # crear un generador de pedidos
 # para que gestione el menú
+require_relative 'generador_productos'
+
+# Creando el inventario
+inventario = []
+10.times do
+    inventario << GeneradorProductos.generar
+end
+
+# Mostrar inventario al usuario
+puts "===== \t Su tienda amiga \t ====="
+puts "Inventario: 📦"
+inventario.each do |producto|
+    puts "#{producto.codigo} \t #{producto.nombre} \t #{producto.precio} \t #{producto.marca} \t #{producto.presentacion} \t #{producto.categoria}"
+end
